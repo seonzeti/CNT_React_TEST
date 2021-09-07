d0be5fe72f6d4f78836ef99f68ce53ed44a7c440
seonzeti <seonzeroticket@gmail.com>
2021-09-07 11:18:40 +0900

    - contact jsx 태그

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,9 +3,37 @@ import React, { PureComponent } from 'react';
 class Contact extends PureComponent {
 render() {
     return (
-        <p>contact</p>
+        <><section>
+            <div>
+                <span>
+                </span>
+                <h3>Email</h3>
+                <a href="#">
+                    seonzeroticket@gmail.com
+                </a>
+            </div>
+        </section><section>
+                <div>
+                    <span>
+                    </span>
+                    <h3>Phone</h3>
+                    <a>
+                        010-3778-5741
+                    </a>
+                </div>
+            </section><section>
+                <div>
+                    <span>
+                    </span>
+                    <h3>Address</h3>
+                    <a>
+                        Somewhere Road #2 
+                        Namgu, Gwangju
+                    </a>
+                </div>
+            </section></>
     );
-  }
+}
 }
 
 export default Contact;
