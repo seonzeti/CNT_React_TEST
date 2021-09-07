35cc9e7efe01dff1f68328e446216532095b1c41
seonzeti <seonzeroticket@gmail.com>
2021-09-07 11:22:22 +0900

    - footer jsx 태그

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,9 +3,25 @@ import React, { PureComponent } from 'react';
 class Footer extends PureComponent {
 render() {
     return (
-      <p>Footer!</p>
+
+        <><div>
+            <ul>
+                <li>twitter</li>
+                <li>facebook</li>
+                <li>instagram</li>
+                <li>github</li>
+                <li>infrun</li>
+            </ul>
+        </div><div>
+                <ul>
+                    <li>(주)커넥트</li>
+                    <li>Design <a href="#"> HTML5UP</a></li>
+                </ul>
+            </div></>
+
+
     );
-  }
+}
 }
 
 export default Footer;
