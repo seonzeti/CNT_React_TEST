490440d36ac9e403a3f4362daa080b35032ff06c
seonzeti <seonzeroticket@gmail.com>
2021-09-07 11:02:22 +0900

    - inner jsx 태그 구성

--- src/components/inner.jsx
+++ src/components/inner.jsx
@@ -3,9 +3,30 @@ import React, { PureComponent } from 'react';
 class Inner extends PureComponent {
 render() {
     return (
-      <p>Inner!</p>
+        <>
+        <header>
+            <h2>전달사항</h2>
+        </header>
+
+        <p>
+        미디어 쿼리의 반응형을 위주로 작업해주세요!
+        SCSS 또는 Style-Component 중 하나를 선택해서 통일성 있게 작업해주세요!
+        스타일 Layout으로는 Flex를 선호합니다.(여러분의 취향존중)
+        아래의 Contact 영역은 폼의 형태만 갖추어 주시고 기능은 전혀 고려하지 않습니다.
+        </p>
+        
+        <p>
+        개발직군에 한해 UI디자인은 고려하지 않고 CODE의 디자인으로만 평가합니다.
+        협업을 하실 (Clean )CODE에 힘을 다 쏟아넣어주세요
+        </p>
+
+        <ul>
+            <li><a href="#">GET STARTED</a></li>
+        </ul></>
+
+
     );
-  }
+}
 }
 
 export default Inner;
