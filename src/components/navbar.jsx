b4e90caee0b0d181e19bd5039c62a9c5e6586d66
seonzeti <seonzeroticket@gmail.com>
2021-09-07 10:47:05 +0900

    - navbar jsx 태그 입력

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,18 @@ import React, { PureComponent } from 'react';
 class Navbar extends PureComponent {
 render() {
     return (
-      <p>Navbar!</p>
+      <><a className="logo">
+        <strong>Connect</strong>
+        <span>React 코딩테스트</span>
+      </a>
+      
+      <nav>
+        <span>로그인해주세요</span>
+        <a href = "#">menu</a>
+      </nav>
+      
+      </>
+      
     );
   }
 }
