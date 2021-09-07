603147544a9b776e1c67823f2e44ec382ba03fa3
seonzeti <seonzeroticket@gmail.com>
2021-09-07 14:48:16 +0900

    - switchText() 함수형으로 변경

--- src/components/major.jsx
+++ src/components/major.jsx
@@ -5,21 +5,31 @@ import axios from 'axios';
 const Major = memo (({ getStarted }) => {
 
 
-    let text = '이 데모용 웹페이지는 Html로 만들어 졌습니다. React로 코딩해주세요! 오른쪽의 버튼을 누르면 Axios 된 값으로 이곳의 글자를 변경해주세요';
-  
-    const switchText = async() => {
-    const connect = await axios.get('https://heronoah.github.io/CNT_Web_TEST_Ref/test-value/');
-    const data = JSON.stringify(connect);
     
-    const obj = JSON.parse(data, (key,value) => {
-        // console.log(`key : ${key}, value : ${value}`)
-        return value;
-    });
-    
-    console.log(obj.data['title']);
-    console.log(obj.data['description']);
-    console.log(obj.data['count']);
-    };
+
+    async function switchText() {
+
+      console.log("123123")
+
+      const connect = await axios.get('https://heronoah.github.io/CNT_Web_TEST_Ref/test-value/');
+      const data = JSON.stringify(connect);
+      
+      const obj = JSON.parse(data, (key,value) => {
+          return value;
+      });
+      
+      console.log(obj.data['title']);
+      console.log(obj.data['description']);
+      console.log(obj.data['count']);        
+
+    }
+
+
+    function text() {
+      return '이 데모용 웹페이지는 Html로 만들어 졌습니다. React로 코딩해주세요! 오른쪽의 버튼을 누르면 Axios 된 값으로 이곳의 글자를 변경해주세요'
+      
+    }
+
 
 
 
@@ -30,7 +40,7 @@ const Major = memo (({ getStarted }) => {
         <h1>"Hi, Let the games Begin"</h1>
       </header><div>
           <p>
-            {text}
+            {text()}
           </p>
 
           <ul>
