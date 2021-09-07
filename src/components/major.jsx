414ec202b1d1bc5daaabf2a87918824d54d93bb7
seonzeti <seonzeroticket@gmail.com>
2021-09-07 10:47:14 +0900

    - major jsx 태그 입력

--- src/components/major.jsx
+++ src/components/major.jsx
@@ -3,7 +3,30 @@ import React, { PureComponent } from 'react';
 class Major extends PureComponent {
 render() {
     return (
-      <p>Major!</p>
+      <>
+      
+      <header>
+        <h1>"Hi, Let the games Begin"</h1>
+      </header>
+      
+      <div>
+        <p>
+          이 데모용 웹페이지는 Html로 만들어 졌습니다. React로 코딩해주세요!      
+          오른쪽의 버튼을 누르면 Axios 된 값으로 이곳의 글자를 변경해주세요
+        </p>
+
+        <ul>
+          <li>
+            <a href="#" className="button next scrolly">
+              GET STARTED
+            </a>
+          </li>
+        </ul>
+
+
+      </div>
+      
+      </>
     );
   }
 }
