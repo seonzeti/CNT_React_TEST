c1270cf6f61a4d742658fe6e18dbbe81efbcee28
seonzeti <86936140+seonzeti@users.noreply.github.com>
2021-09-07 22:01:48 +0900

    - count 수 증가

--- src/components/major.jsx
+++ src/components/major.jsx
@@ -1,38 +1,25 @@
 import React, { memo, useState } from 'react';
 import axios from 'axios';
 
-
-
-
-
 const Major = memo (() => {
 
   const [txt, setTxt] = useState({
     title : "Hi, Let the games Begin",
     description: "이 데모용 웹페이지는 Html로 만들어 졌습니다. React로 코딩해주세요! 오른쪽의 버튼을 누르면 Axios 된 값으로 이곳의 글자를 변경해주세요",
-    count : "0",
+
   });
 
-  // const [count, setCount] = useState(0);
-
+  const [count , setCount] = useState(0);
 
   //데이터 가져오기 
   const getTxt = async () => {
-
     const TXT = await axios('https://heronoah.github.io/CNT_Web_TEST_Ref/test-value/');
-    console.log(TXT.data)
     setTxt(TXT.data)
-
-
   }
-
-
 
     async function SwitchText() {
       getTxt();
-
     }
-
 
     return (
   
@@ -43,17 +30,14 @@ const Major = memo (() => {
           <p>
             {txt.description}
           </p>
-          <p>{txt.count}</p>
+          <p>{count}</p>
 
           <ul>
             <li>           
               <button onClick={SwitchText}>Get Started</button>
-              {/* <button onClick={()=>setTxt.count(count+1)}>increase</button> */}
-
+              <button onClick={()=>setCount(count+1)}>increase</button>
             </li>
           </ul>
-
-
         </div></>
 
     );
