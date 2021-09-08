af1828e8b602b578343a98a23d9d535ca93ecb0e
seonzeti <seonzeroticket@gmail.com>
2021-09-08 10:00:58 +0900

    - 로그인 데이터 들어왔는지 확인작업

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,27 +1,51 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import axios from "axios";
 
 const Login = memo(props => {
-  const onSubmit = event => {};
+  //보관할 초기 useState 설정
+  const [user, setUser] = useState({
+    id: "",
+    pw: "",
+    name: "없음",
+  });
+
+  //유저 데이터 가져오기
+  const getUser = async () => {
+    const User = await axios(
+      "https://heronoah.github.io/CNT_Web_TEST_Ref/login/"
+    );
+    setUser(User.data);
+  };
+
+  const onSubmit = event => {
+    getUser();
+  };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className="fields">
-        <div className="field half">
-          <label>name</label>
-          <input type="text"></input>
-        </div>
-        <div className="field half">
-          <label>password</label>
-          <input type="password"></input>
-        </div>
-      </div>
+    <>
+      <p>{user.name}</p>
 
-      <ul>
-        <li>
-          <input type="submit" value="Login" />
-        </li>
-      </ul>
-    </form>
+      <button onClick={onSubmit}>데이터 들어오는지 확인하기</button>
+
+      {/* <form onSubmit={onSubmit}>
+        <div className="fields">
+          <div className="field half">
+            <label>name</label>
+            <input type="text"></input>
+          </div>
+          <div className="field half">
+            <label>password</label>
+            <input type="password"></input>
+          </div>
+        </div>
+
+        <ul>
+          <li>
+            <input type="submit" value="Login" />
+          </li>
+        </ul>
+      </form> */}
+    </>
   );
 });
 
