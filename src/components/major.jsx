263872edf629fb63b19d301bbfb7283e652f3039
seonzeti <seonzeroticket@gmail.com>
2021-09-08 17:57:12 +0900

    - IncreaseCount 수정 중

--- src/components/major.jsx
+++ src/components/major.jsx
@@ -6,9 +6,8 @@ const Major = memo(() => {
     title: "Hi, Let the games Begin",
     description:
       "이 데모용 웹페이지는 Html로 만들어 졌습니다. React로 코딩해주세요! 오른쪽의 버튼을 누르면 Axios 된 값으로 이곳의 글자를 변경해주세요",
+    count: "0",
   });
-
-  const [count, setCount] = useState(0);
 
   //데이터 가져오기
   const getTxt = async () => {
@@ -23,6 +22,17 @@ const Major = memo(() => {
     getTxt();
   }
 
+  // count를 내가 따로 빼지않고 불러온 데이터의 값을 이용해야함.. 수정수정
+  // 여기서 title, description은 그대로 둔 채로 count만 하고 싶은데
+  // 1)  setTxt.count(txt.count + 1); >> 함수가 아니라고 오류
+
+  const IncreaseCount = () => {
+    setTxt(txt.count + 1);
+    console.log(txt.count);
+    // 2) title, description 까지 같이 수정돼서 공백으로 처리 됨..
+    // >> 이 부분 부터 다시 !!
+  };
+
   return (
     <>
       <header>
@@ -30,12 +40,12 @@ const Major = memo(() => {
       </header>
       <div>
         <p>{txt.description}</p>
-        <p>{count}</p>
+        <p>{txt.count}</p>
 
         <ul>
           <li>
             <button onClick={SwitchText}>Get Started</button>
-            <button onClick={() => setCount(count + 1)}>increase</button>
+            <button onClick={IncreaseCount}>increase</button>
           </li>
         </ul>
       </div>
