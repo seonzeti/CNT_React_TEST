93344f2a7a7e3c064fb37c42db079045af990e44
seonzeti <seonzeroticket@gmail.com>
2021-09-08 11:47:12 +0900

    - 로그인 기능 진행 중

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,41 +1,62 @@
-import React, { memo, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import axios from "axios";
 
 const Login = memo(props => {
   //보관할 초기 useState 설정
-  const [user, setUser] = useState({
-    id: "",
-    pw: "",
-    name: "없음",
-  });
+
+  const [user, setUser] = useState({ id: "", pw: "", name: "" });
+  const [admin, setAdmin] = useState({ id: "", pw: "", name: "" });
 
   //유저 데이터 가져오기
-  const getUser = async () => {
-    const User = await axios(
+  const getAdmin = async () => {
+    const Admin = await axios(
       "https://heronoah.github.io/CNT_Web_TEST_Ref/login/"
     );
-    setUser(User.data);
+    setAdmin(Admin.data);
   };
 
   const onSubmit = event => {
-    getUser();
+    //admin 자료 가져오기
+    getAdmin();
+
+    //입력받은 값으로 user id, pw 를 세팅하고, 가져온 데이터 값과 비교해서 빠꾸시키깅
+    if (user.id == admin.id && user.pw == admin.pw) {
+      console.log("관리자입니다.");
+    } else {
+      console.log("휑.....틀렸어요잉.... ");
+    }
   };
 
   return (
     <>
-      <p>{user.name}</p>
+      <p>{admin.name}</p>
 
       <button onClick={onSubmit}>데이터 들어오는지 확인하기</button>
 
       {/* <form onSubmit={onSubmit}>
         <div className="fields">
           <div className="field half">
-            <label>name</label>
-            <input type="text"></input>
+            <label>
+              ID:
+              <input
+                type="text"
+                name="id"
+                value={user.id || ""}
+                onChange={onChange}
+              />
+            </label>
+            <br />
           </div>
           <div className="field half">
-            <label>password</label>
-            <input type="password"></input>
+            <label>
+              PW:
+              <input
+                type="password"
+                name="pw"
+                value={user.pw || ""}
+                onChange={onChange}
+              />
+            </label>
           </div>
         </div>
 
