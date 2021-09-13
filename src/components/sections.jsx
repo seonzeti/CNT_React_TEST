a77697cb2c10a2beca4ef2dcfa60ec6353403ebe
seonzeti <seonzeroticket@gmail.com>
2021-09-13 10:35:56 +0900

    - node dependencies 1차 정리

--- src/components/sections.jsx
+++ src/components/sections.jsx
@@ -3,74 +3,76 @@ import React from "react";
 const Sections = props => {
   return (
     <>
-      {/* <section id="one" className="tiles">
-        <article>
-          <span className="image">
-            <img src="#" alt="" />
-          </span>
-          <header className="major">
-            <h3>
-              <a href="#banner"></a>
-            </h3>
-            <p>Ipsum dolor sit amet</p>
-          </header>
-        </article>
-        <article>
-          <span className="image">
-            <img src="#" alt="" />
-          </span>
-          <header className="major">
-            <h3>
-              <a href="#banner"></a>
-            </h3>
-            <p>Ipsum dolor sit amet</p>
-          </header>
-        </article>
-        <article>
-          <span className="image">
-            <img src="#" alt="" />
-          </span>
-          <header className="major">
-            <h3>
-              <a href="#banner"></a>
-            </h3>
-            <p>Ipsum dolor sit amet</p>
-          </header>
-        </article>
-        <article>
-          <span className="image">
-            <img src="#" alt="" />
-          </span>
-          <header className="major">
-            <h3>
-              <a href="#banner"></a>
-            </h3>
-            <p>Ipsum dolor sit amet</p>
-          </header>
-        </article>
-        <article>
-          <span className="image">
-            <img src="#" alt="" />
-          </span>
-          <header className="major">
-            <h3>
-              <a href="#banner"></a>
-            </h3>
-            <p>Ipsum dolor sit amet</p>
-          </header>
-        </article>
-        <article>
-          <span className="image">
-            <img src="#" alt="" />
-          </span>
-          <header className="major">
-            <h3>
-              <a href="#banner"></a>
-            </h3>
-            <p>Ipsum dolor sit amet</p>
-          </header>
-        </article>
-      </section> */}
+      {/* <div className="main">
+        <section id="one" className="tiles">
+          <article>
+            <span className="image">
+              <img src="#" alt="" />
+            </span>
+            <header className="major">
+              <h3>
+                <a href="#banner">Section1</a>
+              </h3>
+              <p>Ipsum dolor sit amet</p>
+            </header>
+          </article>
+          <article>
+            <span className="image">
+              <img src="#" alt="" />
+            </span>
+            <header className="major">
+              <h3>
+                <a href="#banner">Section 2</a>
+              </h3>
+              <p>Ipsum dolor sit amet</p>
+            </header>
+          </article>
+          <article>
+            <span className="image">
+              <img src="#" alt="" />
+            </span>
+            <header className="major">
+              <h3>
+                <a href="#banner">Section 3</a>
+              </h3>
+              <p>Ipsum dolor sit amet</p>
+            </header>
+          </article>
+          <article>
+            <span className="image">
+              <img src="#" alt="" />
+            </span>
+            <header className="major">
+              <h3>
+                <a href="#banner">Section 4</a>
+              </h3>
+              <p>Ipsum dolor sit amet</p>
+            </header>
+          </article>
+          <article>
+            <span className="image">
+              <img src="#" alt="" />
+            </span>
+            <header className="major">
+              <h3>
+                <a href="#banner">Section 5</a>
+              </h3>
+              <p>Ipsum dolor sit amet</p>
+            </header>
+          </article>
+          <article>
+            <span className="image">
+              <img src="#" alt="" />
+            </span>
+            <header className="major">
+              <h3>
+                <a href="#banner">Section 6</a>
+              </h3>
+              <p>Ipsum dolor sit amet</p>
+            </header>
+          </article>
+        </section>
+      </div> */}
     </>
   );
 };
